b46111fa5b72d47775b3b042e4f404cc34277ff1
Deivison <rocha0rocha@gmail.com>
2025-06-13 21:13:12 -0300

    adjust persistenc db0base

--- sql/publicacoes.sql
+++ sql/publicacoes.sql
@@ -8,6 +8,17 @@ CREATE TABLE IF NOT EXISTS publicacoes(
     autor_id int not null,
     curtidas int default 0,
     criadoEm timestamp default current_timestamp(),
+    type Publicacao struct {
+	ID          uint64    `json:"id,omitempty"`
+	Titulo      string    `json:"titulo,omitempty"`
+	Conteudo    string    `json:"conteudo,omitempty"`
+	AutorID     uint64    `json:"autorId,omitempty"`
+	AutorNick   string    `json:"autorNick,omitempty"`
+	AutorNome   string    `json:"autorNome,omitempty"`
+	Curtidas    uint64    `json:"curtidas"`
+	Comentarios uint64    `json:"comentarios"`
+	CriadoEm    time.Time `json:"criadoem,omitempty"`
+}
     
     FOREIGN KEY (autor_id) 
     REFERENCES usuarios(id) 
